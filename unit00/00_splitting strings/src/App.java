caa69a6a37ab3d936a7103817167eb0db9780935
J. Evan Noynaert <noynaert@missouriwestern.edu>
2021-12-27 11:23:34 -0600

    Add splitting Strings Demo

--- unit00/00_splitting strings/src/App.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Illustrates splitting strings
- */
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
