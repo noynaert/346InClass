1cfcee0e80980243ec6d61efe2f5e7ebaae7fa7b
J. Evan Noynaert <noynaert@missouriwestern.edu>
2021-12-27 10:12:59 -0600

    Move introductory videos into unit00

---
+++ unit00/00_splitting strings/src/App.java
@@ -0,0 +1,9 @@
+
+/**
+ * Illustrates splitting strings
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
